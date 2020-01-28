a2588f0f2474394d44658737b9ec688fa591aef7
Juliano Tavares <julianotavares@Macbook.local>
2020-01-28 19:36:00 -0300

    Código backend finalizado

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,10 +1,12 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
+const { findConnections, sendMessage } = require('../websocket')
 
 module.exports = {
   async index(req, res) {
     const devs = await Dev.find()
+
     return res.json(devs)
   },
 
@@ -33,8 +35,18 @@ module.exports = {
           techs: techsArray,
           location,
         })
+
+        // Filtrar as conexões que estão no máximo 10km de distância
+        // e o novo Dev tenha pelo menos uma das tecnologias do filtro
+
+        const sendSocketMessageTo = findConnections(
+          { latitude, longitude },
+          techsArray,
+        )
+
+        sendMessage(sendSocketMessageTo, 'new-dev', dev)
       }
     
     return res.json(dev)
-  }
+  },
 }
--- backend/src/models/utils/PointSchema.js
+++ backend/src/models/utils/PointSchema.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+
 const PointSchema = new mongoose.Schema({
   type: {
     type: String,
-    enum: ['Piont'],
+    enum: ['Point'],
     required: true,
   },
   coordinates: {
